39cd31e1608a2897313f043c2473e431315d328f
Xun Zhou <xun.zhou@lidl.com>
2019-06-06 01:12:17 +0200

    convert the text to ios icon

--- README.md
+++ README.md
@@ -66,3 +66,8 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+## Acknowledgments
+
+- css styled ios icon <https://codepen.io/dan10gc/pen/QMBGXP>
+-
--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("de");
+  const [image, setImage] = useState("");
+  const handleOnChange = (e) => {
+    let value = e.target.value.trim();
+    if (value.length > 2) {
+      value = value.substr(0, 2);
+    }
+    setInputValue(value);
+  };
+
+  useEffect(() => {
+    let canvasTxt = document.getElementById("canvasComponent").getContext("2d");
+    canvasTxt.canvas.width = 260;
+    canvasTxt.canvas.height = 260;
+    canvasTxt.font = "160px Major Mono Display";
+    canvasTxt.fillStyle = "#990cab";
+    canvasTxt.fillText(inputValue, 8, 180);
+    canvasTxt.fillRect(0, 0, canvasTxt.width, canvasTxt.height);
+    setImage(canvasTxt.canvas.toDataURL());
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <canvas id="canvasComponent" className="icon calendar"/>
+        {image.length > 0 ? (
+          <img id="imageComponent" alt="icon image" src={image}/>
+        ) : null}
+
+        <p>Enter 2 Characters to generate your iOS icon</p>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleOnChange}
+          placeholder="max. 2 characters"
+        />
       </header>
     </div>
   );
